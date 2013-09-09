71f6be916998f26dd0bcb32acb65de2418e4293a
Harold Asiimwe <harryasiimwe@gmail.com>
2013-09-09 19:06:47 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polltalk::Application.config.secret_token = '0cd019022f791dff2063e87a549700911ab8c0741ce162f637b6ea115a80f9813b620d461c526b9c13ca69243f93ca4f85fba0be20f126fee353ef0be8ea43a8'
